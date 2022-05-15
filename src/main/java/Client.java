b613910d622bde9815104b346f2b85806a633de2
budutaki <takibudu@gmail.com>
2022-05-15 23:18:25 +0300

    Initial commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,24 @@
+public class Client {
+    String name;
+    int age;
+    String sex;
+
+    public Client(String name, int age, String sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return  name;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) {
+        {
+            try {
+                ServerSocket serverSocket = new ServerSocket(8080);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                System.out.println("New connection accepted\n" +
+                        "Write your name");
+
+                final String name = in.readLine();
+
+                out.println(String.format("Hi %s, are you child? (yes/no)", name));
+
+                final String answer = in.readLine();
+
+
+                if (answer.equals("yes")) {
+                    out.println(String.format("Welcome to the kids area, %s, Let's play!", name));
+                }  if (answer.equals("no")) {
+                    out.println(String.format("Welcome to the adult zone, %s, Have a good rest, or a good working day!", name));
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+}
